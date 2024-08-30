86c8e5996f54d243e38aa68210dd3e5055a452ab
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-31 01:52:58 +0700

    extracted into a middleware

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,14 @@
 import { Hono } from "hono";
-import { InteractionResponseType, verifyKey } from "discord-interactions";
+import { InteractionResponseType } from "discord-interactions";
 
 import type { Bindings } from "./bindings";
+import { middleware } from "./middleware";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+app.use(middleware);
+
 app.post("/", async (c) => {
-  const timestamp = c.req.header("X-Signature-Timestamp");
-  const signature = c.req.header("X-Signature-Ed25519");
-
-  if (!timestamp || !signature) {
-    c.status(401);
-    return c.json({ message: "Invalid signature" });
-  }
-
-  const isValid = await verifyKey(
-    await c.req.arrayBuffer(),
-    signature,
-    timestamp,
-    c.env.PUBLIC_KEY,
-  );
-
-  if (!isValid) {
-    c.status(401);
-    return c.json({ message: "Invalid signature" });
-  }
-
   return c.json({ type: InteractionResponseType.PONG });
 });
 
---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { createMiddleware } from "hono/factory";
+import { verifyKey } from "discord-interactions";
+
+import type { Bindings } from "./bindings";
+
+export const middleware = createMiddleware<{ Bindings: Bindings }>(
+  async (c, next) => {
+    const timestamp = c.req.header("X-Signature-Timestamp");
+    const signature = c.req.header("X-Signature-Ed25519");
+
+    if (!timestamp || !signature) {
+      c.status(401);
+      return c.json({ message: "Invalid signature" });
+    }
+
+    const isValid = await verifyKey(
+      await c.req.arrayBuffer(),
+      signature,
+      timestamp,
+      c.env.PUBLIC_KEY,
+    );
+
+    if (!isValid) {
+      c.status(401);
+      return c.json({ message: "Invalid signature" });
+    }
+
+    await next();
+  },
+);
