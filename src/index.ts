12aeb2545166a16598dc6eea9a80d5648c943cfa
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-31 02:15:26 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { InteractionResponseType } from "discord-interactions";
+import { InteractionResponseType, InteractionType } from "discord-interactions";
 
 import type { Bindings } from "./bindings";
 import { middleware } from "./middleware";
@@ -9,7 +9,15 @@ const app = new Hono<{ Bindings: Bindings }>();
 app.use(middleware);
 
 app.post("/", async (c) => {
-  return c.json({ type: InteractionResponseType.PONG });
+  const { type } = await c.req.json<{ type: InteractionType }>();
+
+  switch (type) {
+    case InteractionType.PING:
+      return c.json({ type: InteractionResponseType.PONG });
+    default:
+      c.status(400);
+      return c.json({ message: "Invalid type" });
+  }
 });
 
 export default app;
