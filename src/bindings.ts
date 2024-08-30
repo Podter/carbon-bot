8525b6d994c1f0547e9ce1f8c8f9776c0a40edf0
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-31 01:46:03 +0700

    working ping

---
+++ src/bindings.ts
@@ -0,0 +1,5 @@
+export interface Bindings {
+  APPLICATION_ID: string;
+  PUBLIC_KEY: string;
+  TOKEN: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
 import { Hono } from "hono";
+import { InteractionResponseType, verifyKey } from "discord-interactions";
 
-const app = new Hono();
+import type { Bindings } from "./bindings";
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello, world!" });
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.post("/", async (c) => {
+  const timestamp = c.req.header("X-Signature-Timestamp");
+  const signature = c.req.header("X-Signature-Ed25519");
+
+  if (!timestamp || !signature) {
+    c.status(401);
+    return c.json({ message: "Invalid signature" });
+  }
+
+  const isValid = await verifyKey(
+    await c.req.arrayBuffer(),
+    signature,
+    timestamp,
+    c.env.PUBLIC_KEY,
+  );
+
+  if (!isValid) {
+    c.status(401);
+    return c.json({ message: "Invalid signature" });
+  }
+
+  return c.json({ type: InteractionResponseType.PONG });
 });
 
 export default app;
